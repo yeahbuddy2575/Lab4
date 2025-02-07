7ee880d75f7ac39540d432263428855b29c5e38a
yeahbuddy2575 <josevazquez2575@gmail.com>
2025-02-06 22:55:58 -0500

    Updated Hello.java for Jenkins

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         for(int i = 1; i <= 10; i++) {
-            System.out.println("Hello From Kali" + i);
+            System.out.println("Hello From Jekins" + i);
         }
     }
 }
